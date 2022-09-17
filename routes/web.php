e6dba46c84642f45296ff6070e34cbef4abb3010
Amr Hefny <ahefny1987@gmail.com>
2022-09-17 22:00:51 +0200

    install authentication and bootstrap

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::resource('/images', ImageController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
