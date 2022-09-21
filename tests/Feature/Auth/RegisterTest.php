a210041e7717f394c90b65a0b23d12f42ac8a1f6
Amr Hefny <ahefny1987@gmail.com>
2022-09-21 03:21:42 +0200

    More Login, Register, ImageManagement tests are performed

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
+use App\Models\Image;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
@@ -12,7 +13,7 @@ class RegisterTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function a_new_user_can_be_registered()
+    public function a_new_user_can_be_registered ()
     {
         $response = $this->post('/register', [
             'name' => 'Test',
@@ -30,5 +31,57 @@ class RegisterTest extends TestCase
         
         $this->assertAuthenticated();
         $response->assertRedirect('/');
+    }
+
+    /** @test */
+    public function a_user_can_not_be_register_without_name ()
+    {
+        $response = $this->post('/register', [
+            'name' => '',
+            'email' => 'Test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    public function a_user_can_not_be_register_without_email ()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test',
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['email']);
+    }
+
+    /** @test */
+    public function a_user_can_not_be_register_without_password ()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test',
+            'email' => 'Test@example.com',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+
+        $response->assertSessionHasErrors(['password']);
+    }
+
+    /** @test */
+    public function a_user_can_not_be_register_without_confirming_password ()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test',
+            'email' => 'Test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'wrong password',
+        ]);
+
+        $response->assertSessionHasErrors(['password']);
     }
 }
