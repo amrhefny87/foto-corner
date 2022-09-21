ea66de77729bfefcd401b6afe3baa7d0288e14ad
Amr Hefny <ahefny1987@gmail.com>
2022-09-21 04:23:04 +0200

    Final main push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
     <div id="app">
         <nav class="navbar-class navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
+                <a class="navbar-brand" href="{{ url('/') }}">
                     <img class="logo" src="{{ asset('img/Logo.png') }}">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,7 @@
 <div class="container welcome-container d-flex ">
     <img src="{{ asset('img/Intro.jpg') }}" class="image-intro-body" alt="Intro Image">
     <div class="image-intro-overlay d-flex flex-wrap justify-content-start align-items-center">
-        <div class="image-intro-name">
-            Foto Corner
-        </div>
+            <img class="image-intro-name" src="{{ asset('img/Logo-intro.png') }}" alt="Intro Logo">
     </div>
 </div>
 @endsection
